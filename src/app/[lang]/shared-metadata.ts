432028bcb6c7b3da75cb6e69b8cded3eb850b6d4
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2024-04-01 23:58:36 +0300

    Dev (#13)
    * initial
    
    * added: all base layout
    
    * fixed: gitignore
    
    * fixed: gitignore
    
    * added: env in gitignore
    
    * added: all api routes (#6)
    
    * added: i18n (#8)
    
    * fixed: delete unused page
    
    * fixed: change denomination title
    
    * added: blog/post (#12)

---
+++ src/app/[lang]/shared-metadata.ts
@@ -0,0 +1,17 @@
+export const openGraphBase = {
+  images: [
+    {
+      url: "/images/og_sm.jpg",
+      width: 600,
+      height: 315,
+      alt: "Company logo",
+    },
+    {
+      url: "/images/og_md.jpg",
+      width: 1200,
+      height: 630,
+      alt: "Company logo",
+    },
+  ],
+  type: "website",
+};
