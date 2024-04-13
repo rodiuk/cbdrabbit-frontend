e3c6d94cec1ad968d26cd3ae2409d82210883e10
sashko ss <sashko2307@gmail.com>
2024-04-13 17:18:02 +0300

    before eye password

--- src/Ui/Input/Input.tsx
+++ src/Ui/Input/Input.tsx
@@ -1,66 +1,76 @@
+"use client";
+import React from "react";
+import EyeClosed from "@/components/icons/EyeClosed";
+import EyeOpened from "@/components/icons/EyeOpened";
 
-'use client'
-import React from 'react';
 import cn from "clsx";
 import s from "./s.module.css";
-import EyeClosed from '@/components/icons/EyeClosed';
-import EyeOpened from '@/components/icons/EyeOpened';
 
 interface InputProps {
-	text?: string;
-	required?: boolean;
-	type?: string
-	placeholder?: string
-	password?: boolean
-	isPassword?: boolean
+  text?: string;
+  required?: boolean;
+  type?: string;
+  placeholder?: string;
+  password?: boolean;
+  isPassword?: boolean;
+  value?: string;
+  onInputChange?: (value: string) => void;
 }
 
-const Input: React.FC<InputProps> = ({ text, required, password, isPassword, ...input }) => {
-	
-	const [value, setValue] = React.useState("")
-	const [type, setType] = React.useState(input.type)
-	
-	// const [validate, setValidate] = React.useState(true)
+const Input: React.FC<InputProps> = ({
+  text,
+  required,
+  password,
+  isPassword,
+  value,
+  onInputChange,
+  ...input
+}) => {
+  const [type, setType] = React.useState(input.type);
 
-	const handlerInput = (e: any) => {
-		setValue(e.target.value)
-		//setValidate(true)
-	}
+  const handlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onInputChange) onInputChange(e.target.value);
+  };
 
-	const handkerClick = (el: string) => {
-		setType(el)
-	}
+  const handlerClick = (el: string) => {
+    setType(el);
+  };
 
-	
-
-	return (
-		<label className={ cn(s.label, {
-			error: false
-		})}>
-			<p className={s.label_ttl}>{text}
-				{required && <span className="red">*</span>}
-				{isPassword && <span className={s.right_text}>Забув пароль? {password }</span>}
-			</p>
-			<input className={s.input}
-				{ ...input }
-				onChange={ (e) => handlerInput(e) }
-				type={type}
-				value={value} />
-			{type === "text" ? (
-				<div className={s.pass}
-					onClick={() => handkerClick("password")}
-				>
-					<EyeClosed />
-				</div>
-			) : (
-				<div className={s.pass}
-					onClick={() => handkerClick("text")}
-				>
-					<EyeOpened />
-				</div>)}
-			
-		</label>
-	);
+  return (
+    <label
+      className={cn(s.label, {
+        error: false,
+      })}
+    >
+      <p className={s.label_ttl}>
+        {text}
+        {required && <span className="red">*</span>}
+        {isPassword && (
+          <span className={s.right_text}>Забув пароль? {password}</span>
+        )}
+      </p>
+      <input
+        className={s.input}
+        {...input}
+        onChange={e => handlerInput(e)}
+        type={type}
+        value={value}
+      />
+      {isPassword && (
+        <>
+          {type === "text" ? (
+            <div className={s.pass} onClick={() => handlerClick("password")}>
+              <EyeClosed />
+            </div>
+          ) : (
+            <div className={s.pass} onClick={() => handlerClick("text")}>
+              <EyeOpened />
+            </div>
+          )}
+        </>
+      )}
+    </label>
+  );
 };
 
 export default Input;
--- src/app/[lang]/checkout/page.tsx
+++ src/app/[lang]/checkout/page.tsx
@@ -3,6 +3,75 @@ import React from "react";
 import cn from "clsx";
 import styles from "./page.module.css";
 
+import pic1 from "/public/img/pic1.jpg";
+import Input from "@/Ui/Input/Input";
+
 export default function Checkout() {
-  return <main className={cn("container", styles.main)}>Checkout</main>;
+  return (
+    <main className={cn("container", styles.main)}>
+      <div className={styles.checoutPage}>
+        <div className={styles.checkoutBlocks}>
+          <div className={styles.checkoutBlock}>
+            <div className={styles.checkoutBlock_h2}>Ваше замовлення</div>
+            <ul className={styles.productCheckout}>
+              <li className={styles.list}>
+                <div className={styles.productCheckout_img}>
+                  <img src={pic1.src} alt="pic" />
+                </div>
+                <div className={styles.productCheckout_info}>
+                  <div className={styles.productCheckout_ttl}>
+                    <p>Rabbit Black</p>
+                  </div>
+                  <div className={styles.productCheckout_count}>
+                    <span className={styles.grey}>2 &#215;</span> 80 ₴
+                  </div>
+                </div>
+              </li>
+              <li className={styles.list}>
+                <div className={styles.productCheckout_img}>
+                  <img src={pic1.src} alt="pic" />
+                </div>
+                <div className={styles.productCheckout_info}>
+                  <div className={styles.productCheckout_ttl}>
+                    <p>Rabbit Black</p>
+                  </div>
+                  <div className={styles.productCheckout_count}>
+                    <span className={styles.grey}>2 &#215;</span> 80 ₴
+                  </div>
+                </div>
+              </li>
+            </ul>
+				  </div>
+				  <div className={styles.checkoutBlock}>
+                            <div className={styles.checkoutBlock_h2}>
+                                Контактні дані
+                            </div>
+                            <div className={styles.labels}>
+                                <Input
+                                    type='text'
+                                    name='Телефон'
+                                    text='Телефон'
+                                   // required='true'
+                                    placeholder='+380 (__)___-__-__'
+                                />
+                                <Input
+                                    type='text'
+                                    name='Прізвище'
+                                    text='Прізвище'
+                                    //required='true'
+                                    placeholder='Введи прізвище кирилицею'
+                                />
+                                <Input
+                                    type='text'
+                                    name='Імʼя'
+                                    text='Імʼя'
+                                    //required='true'
+                                    placeholder='Введи імʼя кирилицею'
+                                />
+                            </div>
+                        </div>
+        </div>
+      </div>
+    </main>
+  );
 }
