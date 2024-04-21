818c8e59076a2a01ac4c568b35b7103dbad6a2bb
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2024-04-21 23:07:30 +0300

    Dev (#28)
    * initial
    
    * added: all base layout
    
    * fixed: gitignore
    
    * fixed: gitignore
    
    * added: env in gitignore
    
    * added: all api routes (#6)
    
    * added: i18n (#8)
    
    * fixed: delete unused page
    
    * fixed: change denomination title
    
    * added: blog/post (#12)
    
    * Dev ap routes (#14)
    
    * added: blog/post
    
    * added: send message for new order
    
    * added: main page with logic (#16)
    
    * Ap dev auth (#18)
    
    * registration
    
    * added onclick and onchange
    
    * background fot first page
    
    * /public/img
    
    * before eye password
    
    * added: new header/auth forms
    
    * useMedia
    
    * page checkout is  ready
    
    * added: signIn/signUp flow
    
    ---------
    
    Co-authored-by: sashko ss <sashko2307@gmail.com>
    
    * Ap dev checkout logic (#20)
    
    * before axios
    
    * novaPoshtaSityes врое работает
    
    * checkout
    
    * menu woклы
    
    * page orders ready
    
    * error nova poshta
    
    * before animation for profile
    
    * profile не закончен
    
    * added: checkout
    
    ---------
    
    Co-authored-by: sashko ss <sashko2307@gmail.com>
    
    * Ap dev checkout logic (#22)
    
    * before axios
    
    * novaPoshtaSityes врое работает
    
    * checkout
    
    * menu woклы
    
    * page orders ready
    
    * error nova poshta
    
    * before animation for profile
    
    * profile не закончен
    
    * added: checkout
    
    * fixed: build errors
    
    ---------
    
    Co-authored-by: sashko ss <sashko2307@gmail.com>
    
    * added: profile with logic (#24)
    
    * fixed: build errors
    
    * added: show detail
    
    * added: orders list
    
    * added: terms/policy
    
    ---------
    
    Co-authored-by: sashko ss <sashko2307@gmail.com>

---
+++ src/app/[lang]/terms/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Metadata } from "next";
+import { IMainPageProps } from "@/interfaces/page.interface";
+import { openGraphBase } from "@/app/[lang]/shared-metadata";
+
+import cn from "clsx";
+import styles from "./page.module.css";
+
+export async function generateMetadata({
+  params,
+}: Readonly<IMainPageProps>): Promise<Metadata> {
+  return {
+    alternates: {
+      canonical: `/terms`,
+    },
+    openGraph: {
+      ...openGraphBase,
+      locale: params.lang,
+    },
+  };
+}
+
+export default function Terms() {
+  return <main className={cn("container", styles.main)}>Terms</main>;
+}
