4641baebca9fc4b6386058a08f5488a662bcbdc1
andrii-petlovanyi <andrey.petlovany@gmail.com>
2024-06-06 09:52:36 +0300

    merged

--- src/app/[lang]/errorPage/page.tsx
+++ src/app/[lang]/errorPage/page.tsx
@@ -1,42 +1,25 @@
 import React from "react";
-import { Metadata } from "next";
-import { IMainPageProps } from "@/interfaces/page.interface";
-import { openGraphBase } from "@/app/[lang]/shared-metadata";
+import Image from "next/image";
 
 import cn from "clsx";
 import s from "./page.module.css";
-import Image from "next/image";
 
-import erroeIcon from "/public/img/errorPage.jpg";
+import errorIcon from "/public/img/errorPage.jpg";
 
-export async function generateMetadata({
-  params,
-}: Readonly<IMainPageProps>): Promise<Metadata> {
-  return {
-    alternates: {
-      canonical: `/errorPage`, 
-    },
-    openGraph: {
-      ...openGraphBase,
-      locale: params.lang,
-    },
-  };
-}
-
-export default function ErrorPage() {
+export default function ErrorPage(): React.JSX.Element {
   return (
     <>
       <main className={cn("container", s.main)}>
-			  <div className={s.wrap}>
-			  <div className={s.img}>
-					  <Image src={erroeIcon} alt="iconRubbit" width={510} height={510} />
-				  </div>
-				  <div className={s.text}>
-					  <p className={s.first}>Якась помилка 🙅</p>
-					  <p className={s.two}>Спробуйте пізніше</p>
-				  </div>
-			  </div>
-		 </main>
+        <div className={s.wrap}>
+          <div className={s.img}>
+            <Image src={errorIcon} alt="iconRubbit" width={510} height={510} />
+          </div>
+          <div className={s.text}>
+            <p className={s.first}>Якась помилка 🙅</p>
+            <p className={s.two}>Спробуйте пізніше</p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
