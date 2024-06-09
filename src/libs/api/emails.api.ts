f1bde0b34689b189bdc6a7b40af334a0f557719b
andrii-petlovanyi <andrey.petlovany@gmail.com>
2024-06-09 15:29:56 +0300

    fixed: email

--- src/libs/api/emails.api.ts
+++ src/libs/api/emails.api.ts
@@ -53,7 +53,7 @@ export const passwordResetSendEmail = async (
           first_name: user.firstName,
           last_name: user.lastName,
           lang: lang,
-          url_activator: `https://cbdrabbit.shop/uk/signIn?resetPassword=${code}`,
+          url_password: `https://cbdrabbit.shop/uk/signIn?resetPassword=${code}`,
         }),
       }
     );
