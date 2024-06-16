14c6f67615c9a489c7ad37e9137daea897a162e4
andrii-petlovanyi <andrey.petlovany@gmail.com>
2024-06-16 21:22:31 +0300

    fixed: loyalti

--- src/libs/api/selects/order.select.ts
+++ src/libs/api/selects/order.select.ts
@@ -42,6 +42,7 @@ export const orderSelect: Prisma.OrderSelect = {
     select: {
       id: true,
       quantity: true,
+      productId: true,
       product: {
         select: {
           id: true,
