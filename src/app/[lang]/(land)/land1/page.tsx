6d0ef37416e88470e8cd8afd9f5b30f95fcac2ae
sashko ss <sashko2307@gmail.com>
2024-08-10 19:38:08 +0300

    added links in product name

--- src/app/[lang]/(land)/land1/page.tsx
+++ src/app/[lang]/(land)/land1/page.tsx
@@ -8,11 +8,6 @@ import s from "./page.module.css";
 import { Footer } from "@/components/layout/Footer/Footer";
 import { getDictionary } from "@/libs/18n/getDictionary";
 import { Header } from "@/components/layout/Header/Header";
-import H1Block from "@/components/Landing/H1Block/H1Block";
-import Description2 from "@/components/Landing/Description2/Description2";
-import Accordeon from "@/components/Landing/Accordeon/Accordeon";
-import Buttons from "@/components/Landing/Buttons/Buttons";
-import Link from "next/link";
 
 export async function generateMetadata({
   params,
@@ -32,32 +27,13 @@ export default async function Landingall({ params }: any) {
 	const { lang } = params;
 	const dict = await getDictionary(params.lang);
 	const {header} = dict
-	const landings = [
-		{id: 1, title1: "БІЛИЙ ШОКОЛАД", title2: "КОКОСОВЕ МОЛОКО", title3: "БАНАН 50mg CBD",}
-	]
+	
   return (
 	  <>
 		  <Header lang={params.lang} stylesName="noAfter" />
 		  <main className={cn("container", s.main)}>
         <div className={s.wrapper}>
-          sdBlog has: {landings?.length}
-          <ul>
-            {landings.map(item => {
-              return (
-                <li key={item.id} className={s.item}>
-                  <div className="wrap_item">
-                    <div className={s.date}>{item.title1}</div>
-                    <div className={s.ttl}>
-                      <Link href={`/${lang}/land1/${item.id}`}>
-                        {item.title1}
-                      </Link>
-                    </div>
-                    <div className={s.description}>{item.title2}</div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+          
         </div>
       </main>
       <Footer lang={lang} titles={header.titles} />
