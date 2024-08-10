f059ef5698503dfe844cf70bbd70ab896253aa43
andrii-petlovanyi <andrey.petlovany@gmail.com>
2024-08-10 15:06:44 +0300

    fixed: price changed count

--- src/components/ProductCard/TotalCount/TotalCount.tsx
+++ src/components/ProductCard/TotalCount/TotalCount.tsx
@@ -9,39 +9,47 @@ import cn from "clsx";
 import styles from "./TotalCount.module.css";
 
 interface Props {
-	product: IProductRes;
-	className?: string | undefined
+  product: IProductRes;
+  className?: string | undefined;
 }
 
-export const TotalCount = ({ product, className }: Props): React.JSX.Element => {
+export const TotalCount = ({
+  product,
+  className,
+}: Props): React.JSX.Element => {
   const [products, _] = useAtom(getAllProductsAtom);
   const [, changeCount] = useAtom(changeProductCountAtom);
 
-  const count = products.find(p => p.id === product.id)?.count ?? 0;
+  const count =
+    products.find(p => p.productName === product.productName)?.count ?? 0;
   const [localCount, setLocalCount] = React.useState<string>(count.toString());
 
   React.useEffect(() => {
     setLocalCount(count.toString());
   }, [count]);
 
-  React.useEffect(() => {
-    changeCount({ product, countValue: Number(localCount) });
-  }, [localCount]);
+  // React.useEffect(() => {
+  //   changeCount({ product, countValue: Number(localCount) });
+  // }, [localCount]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (/^\d*$/.test(value)) {
-      setLocalCount(value);
-    } else {
-      setLocalCount("0");
-    }
-  };
+  const handleChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      if (/^\d*$/.test(value)) {
+        setLocalCount(value);
+      } else {
+        setLocalCount("0");
+      }
+      changeCount({ product, countValue: Number(value) });
+    },
+    []
+  );
 
   return (
     <input
       className={cn(styles.container, {
-		  [styles.disabled]: count <= 0,
-		  [styles[className ?? '']]: className
+        [styles.disabled]: count <= 0,
+        [styles[className ?? ""]]: className,
       })}
       value={localCount}
       onChange={handleChange}
