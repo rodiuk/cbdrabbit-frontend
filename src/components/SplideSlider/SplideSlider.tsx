c29ae610865056a4c4ff8976da678d09980f3844
andrii-petlovanyi <andrey.petlovany@gmail.com>
2025-02-21 17:12:46 +0200

    fixed: broken types for package

--- src/components/SplideSlider/SplideSlider.tsx
+++ src/components/SplideSlider/SplideSlider.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import cn from 'clsx';
-import '@splidejs/react-splide/css';
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import cn from "clsx";
+import "@splidejs/react-splide/css";
 interface Props {
   className?: string;
 }
 
 export const SplideSlider: React.FC<Props> = ({ className }) => {
   return (
-	<div className={cn('', className)}>
-	  <Splide aria-label="My Favorite Images">
-  <SplideSlide>
-    <img src="image1.jpg" alt="Image 1"/>
-  </SplideSlide>
-  <SplideSlide>
-    <img src="image2.jpg" alt="Image 2"/>
-  </SplideSlide>
-</Splide>
-	</div>
+    <div className={cn("", className)}>
+      <Splide aria-label="My Favorite Images">
+        <SplideSlide>
+          <img src="image1.jpg" alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="image2.jpg" alt="Image 2" />
+        </SplideSlide>
+      </Splide>
+    </div>
   );
 };
