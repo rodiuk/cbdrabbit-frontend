e1c3465eeaea546b3e6ecd2dcc67fd9f08aae4c0
sashko ss <sashko2307@gmail.com>
2025-02-21 17:07:39 +0200

    error ts1

--- src/components/SplideSlider/SplideSlider.tsx
+++ src/components/SplideSlider/SplideSlider.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import cn from 'clsx';
-
+import '@splidejs/react-splide/css';
 interface Props {
   className?: string;
 }
@@ -8,7 +9,14 @@ interface Props {
 export const SplideSlider: React.FC<Props> = ({ className }) => {
   return (
 	<div className={cn('', className)}>
-	  dvf
+	  <Splide aria-label="My Favorite Images">
+  <SplideSlide>
+    <img src="image1.jpg" alt="Image 1"/>
+  </SplideSlide>
+  <SplideSlide>
+    <img src="image2.jpg" alt="Image 2"/>
+  </SplideSlide>
+</Splide>
 	</div>
   );
 };
