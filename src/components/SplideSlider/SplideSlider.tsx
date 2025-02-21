21a14d756668ee90ab8da6c10e15a4a6e3660e8d
andrii-petlovanyi <andrey.petlovany@gmail.com>
2025-02-21 22:52:09 +0200

    fixed

--- src/components/SplideSlider/SplideSlider.tsx
+++ src/components/SplideSlider/SplideSlider.tsx
@@ -8,32 +8,32 @@ import Image from "next/image";
 import s from "./SplideSlider.module.css";
 
 const images = [
-	"/img/s1.png",
-	"/img/s2.png",
-	"/img/s3.png",
-	"/img/s4.png",
-	"/img/s5.png",
-	"/img/s6.png",
-	"/img/s7.png",
-	"/img/s8.png",
-	  "/img/s9.png",
-	"/img/s10.png",
-	"/img/s11.png",
-	"/img/s12.png",
-	"/img/s13.png",
-	"/img/s14.png",
-	"/img/s15.png",
-	"/img/s16.png",
-	"/img/s17.png",
-	"/img/s18.png",
-	  "/img/s19.png",
-	"/img/s20.png",
-	"/img/s21.png",
-	"/img/s22.png",
-	"/img/s23.png",
-	"/img/s24.png",
-	"/img/s25.png",
-  ];
+  "/img/s1.png",
+  "/img/s2.png",
+  "/img/s3.png",
+  "/img/s4.png",
+  "/img/s5.png",
+  "/img/s6.png",
+  "/img/s7.png",
+  "/img/s8.png",
+  "/img/s9.png",
+  "/img/s10.png",
+  "/img/s11.png",
+  "/img/s12.png",
+  "/img/s13.png",
+  "/img/s14.png",
+  "/img/s15.png",
+  "/img/s16.png",
+  "/img/s17.png",
+  "/img/s18.png",
+  "/img/s19.png",
+  "/img/s20.png",
+  "/img/s21.png",
+  "/img/s22.png",
+  "/img/s23.png",
+  "/img/s24.png",
+  "/img/s25.png",
+];
 
 interface Props {
   className?: string;
@@ -41,38 +41,40 @@ interface Props {
 
 export const SplideSlider: React.FC<Props> = ({ className }) => {
   return (
-	  <div className={cn(s.sliserCont,  {
-		[s.homeslider]: className === "homeslider",
-	})}>
-		  <Splide aria-label="My Favorite Images"
-		  options={{
-			type: "loop",
-			drag: "free",
-			focus: "center",
-			  perPage: 7,
-			  arrows: false, 
-			  pagination: false,
-			autoScroll: {
-			  speed: 1,
-			  },
-			  breakpoints: {
-				1440: { perPage: 6 }, 
-				1200: { perPage: 5 }, 
-				1100: { perPage: 4 }, 
-				768: { perPage: 3 },  
-				640: { perPage: 2 },  
-				580: { perPage: 2 },  
-			  }
-		  }}
-			  extensions={{ AutoScroll }} 
-		  >
-			  {images.map((src, index) => (
-			  <SplideSlide key={index}>
-			  <div className={s.slide}>
-            <Image src={src} alt="slide" width={200} height={150} />
-          </div>
-			</SplideSlide>
-			
+    <div
+      className={cn(s.sliserCont, {
+        [s.homeslider]: className === "homeslider",
+      })}
+    >
+      <Splide
+        aria-label="My Favorite Images"
+        options={{
+          type: "loop",
+          drag: "free",
+          focus: "center",
+          perPage: 7,
+          arrows: false,
+          pagination: false,
+          autoScroll: {
+            speed: 1,
+          },
+          breakpoints: {
+            1440: { perPage: 6 },
+            1200: { perPage: 5 },
+            1100: { perPage: 4 },
+            768: { perPage: 3 },
+            640: { perPage: 2 },
+            580: { perPage: 2 },
+          },
+        }}
+        extensions={{ AutoScroll }}
+      >
+        {images.map((src, index) => (
+          <SplideSlide key={index}>
+            <div className={s.slide}>
+              <Image src={src} alt="slide" width={200} height={150} />
+            </div>
+          </SplideSlide>
         ))}
       </Splide>
     </div>
