0a410ba79c9f2e04d8a1111653c6eecc098d1dec
@andrii_petlovanyi <andrey.petlovany@gmail.com>
2025-06-28 00:01:52 +0300

    changed middleware

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
 import { handler } from "@/configs/auth.config";
 
-export const runtime = "edge"; // запускаємо на Edge runtime
+export const runtime = "nodejs"; // запускаємо на Edge runtime
 export const dynamic = "force-dynamic"; // вимикаємо будь-яку статичну оптимізацію
 export const revalidate = 0; // нульова реалідація (не використовує ISR)
-export const cache = "no-store"; // заголовок Cache-Control: no-store
 
 export { handler as GET, handler as POST };
